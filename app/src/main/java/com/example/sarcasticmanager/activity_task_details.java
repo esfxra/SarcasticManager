161d55a211193bdc720add7145d0c6742ce4676b
Diego Serrano <diegoserranor@gmail.com>
2016-10-15 20:14:20 -0400

    Created activity_task_details and finished up a draft for the UI. Note: Still has warnings related to location of strings***

---
+++ app/src/main/java/com/example/sarcasticmanager/activity_task_details.java
@@ -0,0 +1,13 @@
+package com.example.sarcasticmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_task_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_details);
+    }
+}
