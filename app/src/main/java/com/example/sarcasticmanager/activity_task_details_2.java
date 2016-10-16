f3519173cb34d2940f32cdcf0be6d524c6a3eb71
master-faz <runing456@gmail.com>
2016-10-16 08:41:56 -0400

    link button add. Needs still not linked properly.

---
+++ app/src/main/java/com/example/sarcasticmanager/activity_task_details_2.java
@@ -0,0 +1,32 @@
+package com.example.sarcasticmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class activity_task_details_2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_details);
+    }
+/**
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.second_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }**/
+}
